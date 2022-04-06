c3a47ca95dd25e3abf67e1e0bd12ea2189ae1e6e
root <mtrhoades0727@gmail.com>
2022-04-06 11:23:46 -1000

    added rainbow colors and input box to pick color

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './components/ColorBlock'
+import ColorForm from './components/ColorForm'
+
+export default function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+    
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+    
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    )
+
+}
+
+
---
+++ src/components/ColorBlock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ColorBlock(props){
+    return (
+        <div className="colorBlock"
+        style={{'backgroundColor': props.color}}>
+            <p>{props.color}</p>
+        </div>
+    )
+}
+
+ 
